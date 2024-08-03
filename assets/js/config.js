305e85aae846c1c61ff2ccb561409b6b9a7f28dc
francismelendez134@gmail.com <francismelendez134@gmail.com>
2024-08-02 22:43:21 -0600

    bugfix 2

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,12 @@
 // config.js
 const config = {
-    
+    apiKey: "AIzaSyDvl_koSWKCARcUZUZQ02J1-OKQeoHmLGQ",
+    authDomain: "portafolio-41e1c.firebaseapp.com",
+    projectId: "portafolio-41e1c",
+    storageBucket: "portafolio-41e1c.appspot.com",
+    messagingSenderId: "283651430287",
+    appId: "1:283651430287:web:64014a6190e2e81019ddfa",
+    measurementId: "G-MZR6XERDR3"
   };
   
   export default config;
